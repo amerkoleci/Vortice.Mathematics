60bc2c12847e76cf3647005e68c20d5c8d78c02d
Amer Koleci <amerkoleci@gmail.com>
2019-03-28 08:27:48 +0100

    Revert "Rename some types with better naming and refactoring."
    This reverts commit 8887a3d25effc77019eac9ac164c2b5b7e65b226.

---
+++ src/Vortice.Mathematics/Point.cs
@@ -0,0 +1,134 @@
+﻿// Copyright (c) Amer Koleci and contributors.
+// Distributed under the MIT license. See the LICENSE file in the project root for more information.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Numerics.Hashing;
+using System.Runtime.Serialization;
+
+namespace Vortice.Mathematics
+{
+    /// <summary>
+    /// Defines a 2D integer point.
+    /// </summary>
+    [Serializable]
+    [DataContract]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Point : IEquatable<Point>
+    {
+        /// <summary>
+        /// The size in bytes of the <see cref="Point"/> type.
+        /// </summary>
+        public static readonly int SizeInBytes = Unsafe.SizeOf<Point>();
+
+        /// <summary>
+        /// Represents a <see cref="Point"/> that has X and Y values set to zero.
+        /// </summary>
+        public static readonly Point Empty = new Point();
+
+        /// <summary>
+        /// Gets or sets the x-coordinate of this <see cref="Point"/>.
+        /// </summary>
+        public int X;
+
+        /// <summary>
+        /// Gets or sets the y-coordinate of this <see cref="Point"/>.
+        /// </summary>
+        public int Y;
+
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="Point"/> is empty.
+        /// </summary>
+        public bool IsEmpty => Equals(Empty);
+
+        /// <summary>
+		/// Initializes a new instance of the <see cref="Point"/> struct.
+		/// </summary>
+		/// <param name="x">The x-coordinate.</param>
+		/// <param name="y">The y-coordinate.</param>
+		public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Deconstruct(out int x, out int y)
+        {
+            x = X;
+            y = Y;
+        }
+
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="Point"/> to <see cref="System.Drawing.Point"/>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static implicit operator System.Drawing.Point(Point value)
+        {
+            return new System.Drawing.Point(value.X, value.Y);
+        }
+
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="System.Drawing.Point"/> to <see cref="Point"/>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static implicit operator Point(System.Drawing.Point value)
+        {
+            return new Point(value.X, value.Y);
+        }
+
+        /// <inheritdoc/>
+		public override bool Equals(object obj) => obj is Point point && Equals(ref point);
+
+        /// <summary>
+        /// Determines whether the specified <see cref="Point"/> is equal to this instance.
+        /// </summary>
+        /// <param name="other">The <see cref="Point"/> to compare with this instance.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(Point other) => Equals(ref other);
+
+        /// <summary>
+		/// Determines whether the specified <see cref="Point"/> is equal to this instance.
+		/// </summary>
+		/// <param name="other">The <see cref="Point"/> to compare with this instance.</param>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(ref Point other)
+        {
+            return X == other.X
+                && Y == other.Y;
+        }
+
+        /// <summary>
+        /// Compares two <see cref="Point"/> objects for equality.
+        /// </summary>
+        /// <param name="left">The <see cref="Point"/> on the left hand of the operand.</param>
+        /// <param name="right">The <see cref="Point"/> on the right hand of the operand.</param>
+        /// <returns>
+        /// True if the current left is equal to the <paramref name="right"/> parameter; otherwise, false.
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator ==(Point left, Point right) => left.Equals(ref right);
+
+        /// <summary>
+        /// Compares two <see cref="Point"/> objects for inequality.
+        /// </summary>
+        /// <param name="left">The <see cref="Point"/> on the left hand of the operand.</param>
+        /// <param name="right">The <see cref="Point"/> on the right hand of the operand.</param>
+        /// <returns>
+        /// True if the current left is unequal to the <paramref name="right"/> parameter; otherwise, false.
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator !=(Point left, Point right) => !left.Equals(ref right);
+
+        /// <inheritdoc/>
+		public override int GetHashCode() => HashHelpers.Combine(X.GetHashCode(), Y.GetHashCode());
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return $"Point [ X={X}, Y={Y} ]";
+        }
+    }
+}
--- src/Vortice.Mathematics/ViewportF.cs
+++ src/Vortice.Mathematics/ViewportF.cs
@@ -10,37 +10,37 @@ using System.Runtime.Serialization;
 namespace Vortice.Mathematics
 {
     /// <summary>
-    /// Represents a integer viewport struct.
+    /// Represents a floating-point viewport struct.
     /// </summary>
     [Serializable]
     [DataContract]
     [StructLayout(LayoutKind.Sequential, Pack = 4)]
-    public struct Viewport : IEquatable<Viewport>
+    public struct ViewportF : IEquatable<ViewportF>
     {
         /// <summary>
-        /// The size in bytes of the <see cref="Viewport"/> type.
+        /// The size in bytes of the <see cref="ViewportF"/> type.
         /// </summary>
-        public static readonly int SizeInBytes = Unsafe.SizeOf<Viewport>();
+        public static readonly int SizeInBytes = Unsafe.SizeOf<ViewportF>();
 
         /// <summary>
         /// Position of the pixel coordinate of the upper-left corner of the viewport.
         /// </summary>
-        public int X;
+        public float X;
 
         /// <summary>
         /// Position of the pixel coordinate of the upper-left corner of the viewport.
         /// </summary>
-        public int Y;
+        public float Y;
 
         /// <summary>
         /// Width dimension of the viewport.
         /// </summary>
-        public int Width;
+        public float Width;
 
         /// <summary>
         /// Height dimension of the viewport.
         /// </summary>
-        public int Height;
+        public float Height;
 
         /// <summary>
         /// Gets or sets the minimum depth of the clip volume.
@@ -53,14 +53,14 @@ namespace Vortice.Mathematics
         public float MaxDepth;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Viewport"/> struct.
+        /// Initializes a new instance of the <see cref="ViewportF"/> struct.
         /// </summary>
         /// <param name="width">The width of the viewport in pixels.</param>
         /// <param name="height">The height of the viewport in pixels.</param>
-        public Viewport(int width, int height)
+        public ViewportF(float width, float height)
         {
-            X = 0;
-            Y = 0;
+            X = 0.0f;
+            Y = 0.0f;
             Width = width;
             Height = height;
             MinDepth = 0.0f;
@@ -68,13 +68,13 @@ namespace Vortice.Mathematics
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Viewport"/> struct.
+        /// Initializes a new instance of the <see cref="ViewportF"/> struct.
         /// </summary>
         /// <param name="x">The x coordinate of the upper-left corner of the viewport in pixels.</param>
         /// <param name="y">The y coordinate of the upper-left corner of the viewport in pixels.</param>
         /// <param name="width">The width of the viewport in pixels.</param>
         /// <param name="height">The height of the viewport in pixels.</param>
-        public Viewport(int x, int y, int width, int height)
+        public ViewportF(float x, float y, float width, float height)
         {
             X = x;
             Y = y;
@@ -85,7 +85,7 @@ namespace Vortice.Mathematics
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Viewport"/> struct.
+        /// Initializes a new instance of the <see cref="ViewportF"/> struct.
         /// </summary>
         /// <param name="x">The x coordinate of the upper-left corner of the viewport in pixels.</param>
         /// <param name="y">The y coordinate of the upper-left corner of the viewport in pixels.</param>
@@ -93,7 +93,7 @@ namespace Vortice.Mathematics
         /// <param name="height">The height of the viewport in pixels.</param>
         /// <param name="minDepth">The minimum depth of the clip volume.</param>
         /// <param name="maxDepth">The maximum depth of the clip volume.</param>
-        public Viewport(int x, int y, int width, int height, float minDepth, float maxDepth)
+        public ViewportF(float x, float y, float width, float height, float minDepth, float maxDepth)
         {
             X = x;
             Y = y;
@@ -104,10 +104,10 @@ namespace Vortice.Mathematics
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Viewport"/> struct.
+        /// Initializes a new instance of the <see cref="ViewportF"/> struct.
         /// </summary>
-        /// <param name="bounds">A <see cref="Viewport"/> that defines the location and size of the viewport in a render target.</param>
-        public Viewport(Rectangle bounds)
+        /// <param name="bounds">A <see cref="RectangleF"/> that defines the location and size of the viewport in a render target.</param>
+        public ViewportF(RectangleF bounds)
         {
             X = bounds.X;
             Y = bounds.Y;
@@ -118,15 +118,15 @@ namespace Vortice.Mathematics
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Viewport"/> struct.
+        /// Initializes a new instance of the <see cref="ViewportF"/> struct.
         /// </summary>
         /// <param name="bounds">A <see cref="Vector4"/> that defines the location and size of the viewport in a render target.</param>
-        public Viewport(Vector4 bounds)
+        public ViewportF(Vector4 bounds)
         {
-            X = (int)bounds.X;
-            Y = (int)bounds.Y;
-            Width = (int)bounds.Z;
-            Height = (int)bounds.W;
+            X = bounds.X;
+            Y = bounds.Y;
+            Width = bounds.Z;
+            Height = bounds.W;
             MinDepth = 0.0f;
             MaxDepth = 1.0f;
         }
@@ -134,9 +134,10 @@ namespace Vortice.Mathematics
         /// <summary>
         /// Gets or sets the bounds of the viewport.
         /// </summary>
-        public Rectangle Bounds
+        /// <value>The bounds.</value>
+        public RectangleF Bounds
         {
-            get => new Rectangle(X, Y, Width, Height);
+            get => new RectangleF(X, Y, Width, Height);
             set
             {
                 X = value.X;
@@ -145,7 +146,7 @@ namespace Vortice.Mathematics
                 Height = value.Height;
             }
         }
-        
+
         /// <summary>
         /// Gets the aspect ratio used by the viewport.
         /// </summary>
@@ -154,60 +155,60 @@ namespace Vortice.Mathematics
         {
             get
             {
-                if (Height != 0)
+                if (!MathHelper.IsZero(Height))
                 {
-                    return Width / (float)Height;
+                    return Width / Height;
                 }
                 return 0f;
             }
         }
 
         /// <inheritdoc/>
-		public override bool Equals(object obj) => obj is Viewport value && Equals(ref value);
+		public override bool Equals(object obj) => obj is ViewportF value && Equals(ref value);
 
         /// <summary>
-        /// Determines whether the specified <see cref="Viewport"/> is equal to this instance.
+        /// Determines whether the specified <see cref="ViewportF"/> is equal to this instance.
         /// </summary>
-        /// <param name="other">The <see cref="Viewport"/> to compare with this instance.</param>
+        /// <param name="other">The <see cref="ViewportF"/> to compare with this instance.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public bool Equals(Viewport other) => Equals(ref other);
+        public bool Equals(ViewportF other) => Equals(ref other);
 
         /// <summary>
-		/// Determines whether the specified <see cref="Viewport"/> is equal to this instance.
+		/// Determines whether the specified <see cref="ViewportF"/> is equal to this instance.
 		/// </summary>
-		/// <param name="other">The <see cref="Viewport"/> to compare with this instance.</param>
+		/// <param name="other">The <see cref="ViewportF"/> to compare with this instance.</param>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public bool Equals(ref Viewport other)
+        public bool Equals(ref ViewportF other)
         {
-            return X == other.X
-                && Y == other.Y
-                && Width == other.Width
-                && Height == other.Height
+            return MathHelper.NearEqual(X, other.X)
+                && MathHelper.NearEqual(Y, other.Y)
+                && MathHelper.NearEqual(Width, other.Width)
+                && MathHelper.NearEqual(Height, other.Height)
                 && MathHelper.NearEqual(MinDepth, other.MinDepth)
                 && MathHelper.NearEqual(MaxDepth, other.MaxDepth);
         }
 
         /// <summary>
-        /// Compares two <see cref="Viewport"/> objects for equality.
+        /// Compares two <see cref="ViewportF"/> objects for equality.
         /// </summary>
-        /// <param name="left">The <see cref="Viewport"/> on the left hand of the operand.</param>
-        /// <param name="right">The <see cref="Viewport"/> on the right hand of the operand.</param>
+        /// <param name="left">The <see cref="ViewportF"/> on the left hand of the operand.</param>
+        /// <param name="right">The <see cref="ViewportF"/> on the right hand of the operand.</param>
         /// <returns>
         /// True if the current left is equal to the <paramref name="right"/> parameter; otherwise, false.
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool operator ==(Viewport left, Viewport right) => left.Equals(ref right);
+        public static bool operator ==(ViewportF left, ViewportF right) => left.Equals(ref right);
 
         /// <summary>
-        /// Compares two <see cref="Viewport"/> objects for inequality.
+        /// Compares two <see cref="ViewportF"/> objects for inequality.
         /// </summary>
-        /// <param name="left">The <see cref="Viewport"/> on the left hand of the operand.</param>
-        /// <param name="right">The <see cref="Viewport"/> on the right hand of the operand.</param>
+        /// <param name="left">The <see cref="ViewportF"/> on the left hand of the operand.</param>
+        /// <param name="right">The <see cref="ViewportF"/> on the right hand of the operand.</param>
         /// <returns>
         /// True if the current left is unequal to the <paramref name="right"/> parameter; otherwise, false.
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool operator !=(Viewport left, Viewport right) => !left.Equals(ref right);
+        public static bool operator !=(ViewportF left, ViewportF right) => !left.Equals(ref right);
 
         /// <inheritdoc/>
 		public override int GetHashCode()
