bdd2037982719bfa3121bbf3a08faed36e97147f
Amer Koleci <amerkoleci@gmail.com>
2019-08-29 22:21:45 +0200

    Improve build, use Nerdbank.GitVersioning for versioning.

---
+++ ci-build.cmd
@@ -0,0 +1,7 @@
+﻿@echo off
+@setlocal
+
+dotnet restore src/Vortice.Mathematics.sln
+
+dotnet pack -c Release src/Vortice.Mathematics/Vortice.Mathematics.csproj
+dotnet pack -c Release src/Vortice.Mathematics.PackedVector/Vortice.Mathematics.PackedVector.csproj
