9fb2b8b059951aef9188c8d7147566ab01602532
Amer Koleci <amerkoleci@gmail.com>
2019-09-05 08:54:00 +0200

    Bump to version 1.1.0. Remove Nerdbank.GitVersioning. Add net461 target support.

--- ci-build.cmd
+++
@@ -1,7 +0,0 @@
-﻿@echo off
-@setlocal
-
-dotnet restore src/Vortice.Mathematics.sln
-
-dotnet pack -c Release src/Vortice.Mathematics/Vortice.Mathematics.csproj
-dotnet pack -c Release src/Vortice.Mathematics.PackedVector/Vortice.Mathematics.PackedVector.csproj
