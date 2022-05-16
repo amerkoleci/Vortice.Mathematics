411153579850e3a41c44da72aace637670dae9e6
Amer Koleci <amerkoleci@gmail.com>
2022-05-16 17:35:48 +0200

    Color4: Various improvements.

--- src/Vortice.Mathematics/Color4.cs
+++ src/Vortice.Mathematics/Color4.cs
@@ -5,6 +5,7 @@ using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Diagnostics;
 
 #if NET5_0_OR_GREATER
 using System.Runtime.Intrinsics;
@@ -18,10 +19,10 @@ namespace Vortice.Mathematics;
 /// </summary>
 [Serializable]
 [StructLayout(LayoutKind.Sequential, Pack = 4)]
-public readonly struct Color4 : IEquatable<Color4>, IFormattable
+public struct Color4 : IEquatable<Color4>, IFormattable
 {
 #if NET5_0_OR_GREATER
-    private readonly Vector128<float> _value;
+    private Vector128<float> _value;
 #endif
 
     /// <summary>
@@ -78,7 +79,7 @@ public readonly struct Color4 : IEquatable<Color4>, IFormattable
     /// Initializes a new instance of the <see cref="Color4"/> struct.
     /// </summary>
     /// <param name="value">The red, green, blue, and alpha components of the color.</param>
-    public Color4(Vector4 value)
+    public Color4(in Vector4 value)
     {
 #if NET5_0_OR_GREATER
         _value = value.AsVector128();
@@ -95,7 +96,7 @@ public readonly struct Color4 : IEquatable<Color4>, IFormattable
     /// </summary>
     /// <param name="value">The red, green, and blue components of the color.</param>
     /// <param name="alpha">The alpha component of the color.</param>
-    public Color4(Vector3 value, float alpha)
+    public Color4(in Vector3 value, float alpha)
     {
 #if NET5_0_OR_GREATER
         _value = Vector128.Create(value.X, value.Y, value.Z, alpha);
@@ -112,7 +113,7 @@ public readonly struct Color4 : IEquatable<Color4>, IFormattable
     /// </summary>
     /// <param name="value">The red, green, and blue components of the color.</param>
     /// <param name="alpha">The alpha component of the color.</param>
-    public Color4(Color3 value, float alpha)
+    public Color4(in Color3 value, float alpha)
     {
 #if NET5_0_OR_GREATER
         _value = Vector128.Create(value.R, value.G, value.B, alpha);
@@ -128,7 +129,7 @@ public readonly struct Color4 : IEquatable<Color4>, IFormattable
     /// Initializes a new instance of the <see cref="Color4"/> struct.
     /// </summary>
     /// <param name="color"><see cref="Color"/> used to initialize the color.</param>
-    public Color4(Color color)
+    public Color4(in Color color)
     {
 #if NET5_0_OR_GREATER
         _value = Vector128.Create(color.R / 255.0f, color.G / 255.0f, color.B / 255.0f, color.A / 255.0f);
@@ -138,6 +139,20 @@ public readonly struct Color4 : IEquatable<Color4>, IFormattable
         B = color.B / 255.0f;
         A = color.A / 255.0f;
 #endif
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Color4" /> struct.
+    /// </summary>
+    /// <param name="values">The span of elements to assign to the vector.</param>
+    public Color4(ReadOnlySpan<float> values)
+    {
+        if (values.Length < 4)
+        {
+            throw new ArgumentOutOfRangeException(nameof(values), "There must be 4 uint values.");
+        }
+
+        this = Unsafe.ReadUnaligned<Color4>(ref Unsafe.As<float, byte>(ref MemoryMarshal.GetReference(values)));
     }
 
 #if NET5_0_OR_GREATER
@@ -153,7 +168,7 @@ public readonly struct Color4 : IEquatable<Color4>, IFormattable
     /// <summary>
     /// Red component of the color.
     /// </summary>
-    public float R
+    public readonly float R
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get
@@ -165,7 +180,7 @@ public readonly struct Color4 : IEquatable<Color4>, IFormattable
     /// <summary>
     /// Green component of the color.
     /// </summary>
-    public float G
+    public readonly float G
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get
@@ -177,7 +192,7 @@ public readonly struct Color4 : IEquatable<Color4>, IFormattable
     /// <summary>
     /// Blue component of the color.
     /// </summary>
-    public float B
+    public readonly float B
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get
@@ -189,7 +204,7 @@ public readonly struct Color4 : IEquatable<Color4>, IFormattable
     /// <summary>
     /// Alpha component of the color.
     /// </summary>
-    public float A
+    public readonly float A
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get
@@ -220,6 +235,12 @@ public readonly struct Color4 : IEquatable<Color4>, IFormattable
 
 #endif
 
+    public float this[int index]
+    {
+        readonly get => GetElement(this, index);
+        set => this = WithElement(this, index, value);
+    }
+
     /// <summary>
     /// Converts the color into a packed integer.
     /// </summary>
@@ -230,6 +251,26 @@ public readonly struct Color4 : IEquatable<Color4>, IFormattable
         g = (byte)(G * 255.0f);
         b = (byte)(B * 255.0f);
         a = (byte)(A * 255.0f);
+    }
+
+    /// <summary>
+    /// Determines the negative RGB color value of a color.
+    /// </summary>
+    /// <param name="color">The color value</param>
+    /// <returns>The negative color</returns>
+    public static Color4 Negative(in Color4 color)
+    {
+        return new Color4(1.0f - color.R, 1.0f - color.G, 1.0f - color.B, color.A);
+    }
+
+    /// <summary>
+    /// Blends two colors by multiplying corresponding components together.
+    /// </summary>
+    /// <param name="color1">The first color.</param>
+    /// <param name="color2">The second color.</param>
+    public static Color4 Modulate(in Color4 color1, in Color4 color2)
+    {
+        return Multiply(color1, color2);
     }
 
     /// <summary>
@@ -265,6 +306,158 @@ public readonly struct Color4 : IEquatable<Color4>, IFormattable
             );
     }
 
+    public void Deconstruct(out float red, out float green, out float blue, out float alpha)
+    {
+        red = R;
+        green = G;
+        blue = B;
+        alpha = A;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public readonly void CopyTo(float[] array)
+    {
+        CopyTo(array, 0);
+    }
+
+    public readonly void CopyTo(float[] array, int index)
+    {
+        if (array is null)
+        {
+            throw new NullReferenceException(nameof(array));
+        }
+
+        if ((index < 0) || (index >= array.Length))
+        {
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+
+        if ((array.Length - index) < 4)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+
+        array[index] = R;
+        array[index + 1] = G;
+        array[index + 2] = B;
+        array[index + 3] = A;
+    }
+
+    /// <summary>Copies the vector to the given <see cref="Span{T}" />.The length of the destination span must be at least 2.</summary>
+    /// <param name="destination">The destination span which the values are copied into.</param>
+    /// <exception cref="ArgumentException">If number of elements in source vector is greater than those available in destination span.</exception>
+    public readonly void CopyTo(Span<float> destination)
+    {
+        if (destination.Length < 4)
+        {
+            throw new ArgumentOutOfRangeException(nameof(destination));
+        }
+
+        Unsafe.WriteUnaligned(ref Unsafe.As<float, byte>(ref MemoryMarshal.GetReference(destination)), this);
+    }
+
+    /// <summary>Attempts to copy the vector to the given <see cref="Span{Int32}" />. The length of the destination span must be at least 2.</summary>
+    /// <param name="destination">The destination span which the values are copied into.</param>
+    /// <returns><see langword="true" /> if the source vector was successfully copied to <paramref name="destination" />. <see langword="false" /> if <paramref name="destination" /> is not large enough to hold the source vector.</returns>
+    public readonly bool TryCopyTo(Span<float> destination)
+    {
+        if (destination.Length < 4)
+        {
+            return false;
+        }
+
+        Unsafe.WriteUnaligned(ref Unsafe.As<float, byte>(ref MemoryMarshal.GetReference(destination)), this);
+        return true;
+    }
+
+    /// <summary>
+    /// Returns a new color whose values are the product of each pair of elements in two specified colors.
+    /// </summary>
+    /// <param name="left">The first color.</param>
+    /// <param name="right">The second color.</param>
+    /// <returns>The element-wise product vector.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Color4 Multiply(Color4 left, Color4 right)
+    {
+#if NET5_0_OR_GREATER
+        Vector128<float> result = VectorUtilities.Multiply(left._value, right._value);
+        return new Color4(result);
+#else
+        return new Color4(left.R * right.R, left.G * right.G, left.B * right.B, left.A * right.A);
+#endif
+    }
+
+    /// <summary>Multiplies a color by a specified scalar.</summary>
+    /// <param name="left">The color to multiply.</param>
+    /// <param name="right">The scalar value.</param>
+    /// <returns>The scaled color.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Color4 Multiply(Color4 left, float right)
+    {
+#if NET5_0_OR_GREATER
+        Vector128<float> result = VectorUtilities.Multiply(left._value, right);
+        return new Color4(result);
+#else
+        return new Color4(left.R * right, left.G * right, left.B * right, left.A * right);
+#endif
+    }
+
+    /// <summary>Multiplies a scalar value by a specified color.</summary>
+    /// <param name="left">The scaled value.</param>
+    /// <param name="right">The color.</param>
+    /// <returns>The scaled color.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Color4 Multiply(float left, Color4 right)
+    {
+#if NET5_0_OR_GREATER
+        Vector128<float> result = VectorUtilities.Multiply(right._value, left);
+        return new Color4(result);
+#else
+        return new Color4(left * right.R, left * right.G, left * right.B, left * right.A);
+#endif
+    }
+
+    /// <summary>Computes the product of a color and a float.</summary>
+    /// <param name="left">The vector to multiply by <paramref name="right" />.</param>
+    /// <param name="right">The float which is used to multiply <paramref name="left" />.</param>
+    /// <returns>The product of <paramref name="left" /> multipled by <paramref name="right" />.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Color4 operator *(float left, Color4 right)
+    {
+        return Multiply(left, right);
+    }
+
+    /// <summary>Computes the product of a color and a float.</summary>
+    /// <param name="left">The vector to multiply by <paramref name="right" />.</param>
+    /// <param name="right">The float which is used to multiply <paramref name="left" />.</param>
+    /// <returns>The product of <paramref name="left" /> multipled by <paramref name="right" />.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Color4 operator *(Color4 left, float right)
+    {
+#if NET5_0_OR_GREATER
+        Vector128<float> result = VectorUtilities.Multiply(left._value, right);
+        return new Color4(result);
+#else
+        return new Color4(left.R * right, left.G * right, left.B * right, left.A * right);
+#endif
+    }
+
+    /// <summary>Computes the product of two colors.</summary>
+    /// <param name="left">The color to multiply by <paramref name="right" />.</param>
+    /// <param name="right">The color which is used to multiply <paramref name="left" />.</param>
+    /// <returns>The product of <paramref name="left" /> multipled by <paramref name="right" />.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Color4 operator *(Color4 left, Color4 right)
+    {
+#if NET5_0_OR_GREATER
+        Vector128<float> result = VectorUtilities.Multiply(left._value, right._value);
+        return new Color4(result);
+#else
+        return new Color4(left.R * right.R, left.G * right.G, left.B * right.B, left.A * right.A);
+#endif
+    }
+
+
     /// <summary>
     /// Converts the color to <see cref="Vector3"/>.
     /// </summary>
@@ -278,38 +471,32 @@ public readonly struct Color4 : IEquatable<Color4>, IFormattable
     public Vector4 ToVector4() => new(R, G, B, A);
 
     /// <summary>
-    /// Creates an array containing the elements of the color.
-    /// </summary>
-    /// <returns>A four-element array containing the components of the color.</returns>
-    public float[] ToArray() => new[] { R, G, B, A };
-
-    /// <summary>
     /// Performs an explicit conversion from <see cref="Color4"/> to <see cref="Vector3"/>.
     /// </summary>
     /// <param name="value">The value.</param>
     /// <returns>The result of the conversion.</returns>
-    public static explicit operator Vector3(Color4 value) => new Vector3(value.R, value.G, value.B);
+    public static explicit operator Vector3(Color4 value) => new(value.R, value.G, value.B);
 
     /// <summary>
     /// Performs an implicit conversion from <see cref="Color4"/> to <see cref="Vector4"/>.
     /// </summary>
     /// <param name="value">The value.</param>
     /// <returns>The result of the conversion.</returns>
-    public static implicit operator Vector4(Color4 value) => new Vector4(value.R, value.G, value.B, value.A);
+    public static implicit operator Vector4(Color4 value) => new(value.R, value.G, value.B, value.A);
 
     /// <summary>
     /// Performs an explicit conversion from <see cref="Vector3"/> to <see cref="Color4"/>.
     /// </summary>
     /// <param name="value">The value.</param>
     /// <returns>The result of the conversion.</returns>
-    public static explicit operator Color4(Vector3 value) => new Color4(value.X, value.Y, value.Z, 1.0f);
+    public static explicit operator Color4(Vector3 value) => new(value.X, value.Y, value.Z, 1.0f);
 
     /// <summary>
     /// Performs an explicit conversion from <see cref="Vector4"/> to <see cref="Color4"/>.
     /// </summary>
     /// <param name="value">The value.</param>
     /// <returns>The result of the conversion.</returns>
-    public static explicit operator Color4(Vector4 value) => new Color4(value.X, value.Y, value.Z, value.W);
+    public static explicit operator Color4(Vector4 value) => new(value.X, value.Y, value.Z, value.W);
 
     /// <inheritdoc />
     public override bool Equals(object? obj) => obj is Color4 other && Equals(other);
@@ -378,5 +565,50 @@ public readonly struct Color4 : IEquatable<Color4>, IFormattable
             .Append(A.ToString(format, formatProvider))
             .Append('>')
             .ToString();
+    }
+
+    internal const int Count = 4;
+
+    internal static float GetElement(Color4 vector, int index)
+    {
+        if (index >= Count)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+
+        return GetElementUnsafe(ref vector, index);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static float GetElementUnsafe(ref Color4 vector, int index)
+    {
+        Debug.Assert(index is >= 0 and < Count);
+
+        return Unsafe.Add(ref Unsafe.As<Color4, float>(ref vector), index);
+    }
+
+    /// <summary>Sets the element at the specified index.</summary>
+    /// <param name="vector">The vector of the element to get.</param>
+    /// <param name="index">The index of the element to set.</param>
+    /// <param name="value">The value of the element to set.</param>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="index" /> was less than zero or greater than the number of elements.</exception>
+    internal static Color4 WithElement(Color4 vector, int index, float value)
+    {
+        if ((uint)index >= Count)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+
+        Color4 result = vector;
+        SetElementUnsafe(ref result, index, value);
+        return result;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    internal static void SetElementUnsafe(ref Color4 vector, int index, float value)
+    {
+        Debug.Assert(index is >= 0 and < Count);
+
+        Unsafe.Add(ref Unsafe.As<Color4, float>(ref vector), index) = value;
     }
 }
